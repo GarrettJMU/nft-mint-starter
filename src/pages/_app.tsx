97058508de247ec461bc5b9a3290fa36e98fc603
Garrett Hughes <garretthughes3@gmail.com>
2023-04-12 16:47:23 -0400

    Add in wagmi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {WagmiConfig} from "wagmi";
+import {client} from "@/utils/wagmi";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+      <WagmiConfig client={client}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+  )
 }
---
+++ src/utils/wagmi.ts
@@ -0,0 +1,13 @@
+import { createClient, configureChains, mainnet } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+    [mainnet],
+    [publicProvider()],
+)
+
+export const client = createClient({
+    autoConnect: true,
+    provider,
+    webSocketProvider,
+})
